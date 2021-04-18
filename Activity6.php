027d04c99b872f0e64aaf7d7df77dab85aad35bc
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-04-19 01:08:37 +0300

    Update

---
+++ Activity6.php
@@ -0,0 +1,12 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+	    <title>Activity 6</title>
+    </head>
+    <body>
+        <h1>Currency Converter</h1>  
+    </body>
+</html>
