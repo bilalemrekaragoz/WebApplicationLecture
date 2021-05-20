df5d23c4980c90e19728048738f805464e266274
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-05-21 01:56:58 +0300

    Update

--- Karagoz-Activity7.js
+++ Karagoz-Activity7.js
@@ -3,7 +3,63 @@ var scores = [88, 98, 77, 88];
 
 var $ = function (id) { return document.getElementById(id); };
 
+var addScore = function()
+{
+	if($("score").value>0 && $("score").value<=100 && $("name").value != null)
+	{
+		names.push($("name".value));
+		scores.push($("score").value);
+	}
+	else
+	{
+		alert("you must enter a name and valid score");
+	}
+}
 
+var displayResults = function()
+{
+	var max=0;
+	var sum=0;
+	var index=0;
+	var average=0;
+	var length = scores.length;
+
+	for(var i=0; i<length;i++)
+	{
+		sum = sum + Number(scores[i]);
+		if(scores[i]>max)
+		{
+			max=scores[i];
+			index=i;
+		}
+	}
+
+	average = sum / length;
+
+	$("results").innerHTML = "<h2>Results</h2>";
+	$("results").innerHTML = "<p>Average score: " + average + "</p>";
+	$("results").innerHTML = "</br><p>High score is: "  + names[index] + " with a score of " + max + "</p>";
+}
+
+
+var displayScores = function()
+{
+	var length = scores.length;
+
+	$("scores_table").innerHTML = "<h2>Scores</h2>";
+	$("scores_table").innerHTML = "<tr><th>Name</th><th>Score</th></tr>";
+
+	for(var i=0;i<length;i++)
+	{
+		$("scores_table").innerHTML += "<tr><td>" + names[i] + "</td><td>" + scores[i] + "</td></tr>";
+ 	}	
+}
+
+
+var addScore = function()
+{
+
+}
 
 window.onload = function () {
 	$("add").onclick = addScore;
