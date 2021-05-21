1aaf61ad0f74d12015647e1e478014a59de0401e
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-05-21 03:52:50 +0300

    Update

--- Karagoz-Activity8.js
+++ Karagoz-Activity8.js
@@ -1,11 +1,39 @@
-window.onload()
-{
-    document.getElementById("Button").onclick = changePosition;
-}
-
+var clickNumber=0;
 
 function changePosition()
 {
-    document.getElementById("Button").style.backgroundColor = "red";
+    var randLeft = Math.random()*500;
+    document.getElementById("Button").style.marginLeft = randLeft + "px";
+    var randTop = Math.random()*500;
+    document.getElementById("Button").style.marginTop = randTop + "px";
+}
 
+function setTimer()
+{
+    if(clickNumber <= 3)
+        setTimeout(changePosition,500);
+    else if(clickNumber > 3 && clickNumber <= 6)
+        setTimeout(changePosition,400);
+    else if(clickNumber > 6 && clickNumber <= 9)
+        setTimeout(changePosition,300);
+    else if(clickNumber > 9 && clickNumber <= 12)
+        setTimeout(changePosition,200);
+    else if(clickNumber > 12 )
+        setTimeout(changePosition,100);
+}
+
+function increaseClickNumber()
+{
+    clickNumber++;
+    document.getElementById("text").innerHTML = clickNumber;
+    
+}
+
+
+window.onload = function()
+{
+    document.getElementById("Button").onmouseover = setTimer;
+    document.getElementById("Button").onclick = changePosition;
+    document.getElementById("Button").onclick = increaseClickNumber;
+    clickNumber=0;
 }
