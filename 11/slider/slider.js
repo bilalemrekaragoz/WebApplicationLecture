49802a9e9428cdd3937fa5090a283d29270cf31b
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-06-03 22:41:38 +0300

    Update

--- 11/slider/slider.js
+++ 11/slider/slider.js
@@ -1,16 +1,18 @@
-
-
 $(document).ready(function() {
-	$("#slider").bxSlider({
-	  auto: true,
-	  minSlides: 1,
-	  maxSlides: 1,
-	  slideWidth: 250,
-	  slideMargin: 10,
-	  moveSlides: 1,
+
+    $("#slider").bxSlider({
+		auto: true,
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 250,
+		slideMargin: 10,
+		randomStart: true,
+		moveSlides: true,
 		pause: 3000,
 		pager: true,
 		pagerType: 'short',
-		pagerSelector: '#pager'
+		pagerSelector : '#pager'
+		
 	});
+
 });
