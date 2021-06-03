88430d1c1217f6a7731838f8b1f206a708713517
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-06-03 22:39:42 +0300

    Update

--- 11/slider/slider.js
+++ 11/slider/slider.js
@@ -1,17 +1,16 @@
-$(document).ready(function() 
-{
-    $("#slider").bxSlider(
-	{		
-		auto: true,
-		minSlides: 1,
-		maxSlides: 1,
-		slideWidth: 250,
-		slideMargin: 10,
-		randomStart: true,
-		moveSlides: 1,
+
+
+$(document).ready(function() {
+	$("#slider").bxSlider({
+	  auto: true,
+	  minSlides: 1,
+	  maxSlides: 1,
+	  slideWidth: 250,
+	  slideMargin: 10,
+	  moveSlides: 1,
 		pause: 3000,
 		pager: true,
 		pagerType: 'short',
 		pagerSelector: '#pager'
-	});	
+	});
 });
